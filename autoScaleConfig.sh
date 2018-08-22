84d373913b126dd07ece165fc89612eb455a96a2
Nigel Leck <nigel@stsoftware.com.au>
2018-08-22 21:47:55 +1000

    fixes

--- autoScaleConfig.sh
+++ autoScaleConfig.sh
@@ -17,14 +17,19 @@ set -o nounset
 # set -o xtrace
 
 main() {
-        #Variable declaration (all omitted variables are made empty)
-        config_json="$1"
-        auto_scaling_group_name=$(jq -r '.AutoScalingGroupName // empty' "${config_json}")
-        cost_model=$(jq -r '.CostModel // empty' "${config_json}")
-        instance_type=$(jq -r '.InstanceType // empty' "${config_json}")
-        min_size=$(jq -r '.MinSize // empty' "${config_json}")
-        desired_capacity=$(jq -r '.DesiredCapacity // empty' "${config_json}")
-        max_size=$(jq -r '.MaxSize // empty' "${config_json}")
+    #Variable declaration (all omitted variables are made empty)
+    config_array_file="$1"
+
+    local configArray=$(jq -c '.[]' ${config_array_file})
+
+    for configJSON in $configArray; do
+
+        auto_scaling_group_name=$(jq -r '.AutoScalingGroupName // empty' <<<"${configJson}")
+        cost_model=$(jq -r '.CostModel // empty' <<<"${configJson}")
+        instance_type=$(jq -r '.InstanceType // empty' <<<"${configJson}")
+        min_size=$(jq -r '.MinSize // empty' <<<"${configJson}")
+        desired_capacity=$(jq -r '.DesiredCapacity // empty' <<<"${configJson}")
+        max_size=$(jq -r '.MaxSize // empty' <<<"${configJson}")
 
         # Find launch configuration ID
         auto_scaling_group_json=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-name "${auto_scaling_group_name}")
@@ -49,6 +54,7 @@ main() {
         # (>&2 jq . $jsonFile) 
         aws autoscaling update-auto-scaling-group --auto-scaling-group-name "${auto_scaling_group_name}" --cli-input-json file://${jsonFile}
         rm $jsonFile
+    done
 }
 
 main "$@"
